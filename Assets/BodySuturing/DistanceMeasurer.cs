c27e03cbadf6810bee851576cd22787e32fcb364
InZenFenix <108585945+inzenfenix@users.noreply.github.com>
2024-01-23 15:12:14 -0300

    Added forceps simulation

---
+++ Assets/BodySuturing/DistanceMeasurer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceMeasurer : MonoBehaviour
+{
+    [SerializeField] private Transform point1;
+
+    [SerializeField] private Transform point2;
+
+    private void Start()
+    {
+        Debug.Log(Vector3.Distance(point1.position, point2.position));
+    }
+}
---
+++ Assets/Scripts/AnatomicalForcepsDeform.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnatomicalForcepsDeform : MonoBehaviour
+{
+    [Header("TOP PART OF THE FORCEPS")]
+    [SerializeField] private Transform TopBone;
+
+    public Vector3 originalTopRotation;
+    public Vector3 deformedTopRotation;
+
+    [Range(0f, 1f)]
+    public float topKey;
+
+    [Header("\nBOTTOM PART OF THE FORCEPS")]
+    [SerializeField]private Transform BottomBone;
+
+    public Vector3 originalBottomRotation;
+    public Vector3 deformedBottomRotation;
+
+    [Range(0f, 1f)]
+    public float bottomKey;
+
+    private Quaternion originalTopRotation_Q;
+    private Quaternion deformedlTopRotation_Q;
+
+    private Quaternion originalBottomRotation_Q;
+    private Quaternion deformedlBottomRotation_Q;
+
+    private void Start()
+    {
+        originalTopRotation_Q = Quaternion.Euler(originalTopRotation);
+        deformedlTopRotation_Q = Quaternion.Euler(deformedTopRotation);
+
+        originalBottomRotation_Q = Quaternion.Euler(originalBottomRotation);
+        deformedlBottomRotation_Q = Quaternion.Euler(deformedBottomRotation);
+    }
+
+    private void Update()
+    {
+        TopBone.localRotation = Quaternion.Slerp(originalTopRotation_Q, deformedlTopRotation_Q, topKey);
+        BottomBone.localRotation = Quaternion.Slerp(originalBottomRotation_Q, deformedlBottomRotation_Q, bottomKey);
+    }
+
+}
