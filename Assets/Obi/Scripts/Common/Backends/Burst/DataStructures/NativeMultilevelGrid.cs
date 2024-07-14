37895801146d110b1e4b45a80fb8ea14f71e3579
InZenFenix <108585945+inzenfenix@users.noreply.github.com>
2024-07-13 21:15:34 -0400

    Fixed packages for burst system
    Fixed problem where you have to install packages again everytime manifest.json was changed.

--- Assets/Obi/Scripts/Common/Backends/Burst/DataStructures/NativeMultilevelGrid.cs
+++ Assets/Obi/Scripts/Common/Backends/Burst/DataStructures/NativeMultilevelGrid.cs
@@ -84,15 +84,15 @@ namespace Obi
             }
         }
 
-        public NativeHashMap<int4, int> grid;
+        public NativeParallelHashMap<int4, int> grid;
         public NativeList<Cell<T>> usedCells;
-        public NativeHashMap<int, int> populatedLevels;
+        public NativeParallelHashMap<int, int> populatedLevels;
 
         public NativeMultilevelGrid(int capacity, Allocator label)
         {
-            grid = new NativeHashMap<int4, int>(capacity, label);
+            grid = new NativeParallelHashMap<int4, int>(capacity, label);
             usedCells = new NativeList<Cell<T>>(label);
-            populatedLevels = new NativeHashMap<int, int>(10, label);
+            populatedLevels = new NativeParallelHashMap<int, int>(10, label);
         }
 
         public int CellCount
